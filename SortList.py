81c0069f98a4046879d384cbc0b4721dee7919bd
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-09-04 15:25:00 -0300

    inutil

--- SortList.py
+++ SortList.py
@@ -3,6 +3,7 @@ import sublime_plugin
 
 
 class SortListCommand(sublime_plugin.TextCommand):
+    # blah
     def run(self, edit):
         prepend = append = ''
         region = self.view.sel()[0]
